a978979c500657083aa5a9c1afc3545878d9e4ff
canhnvph03510 <canhnvph03510@fpt.edu.vn>
2016-08-24 14:03:25 +0700

    thanh commit

---
+++ Desktop/shopLaptop/include/footer.php
@@ -0,0 +1,2 @@
+<b>LaptopOnlineStore - Chuyên kinh doanh máy tính xách tay trực tuyến</br>
+Thực hiện: Nhóm 7 - INF 205</b>
---
+++ Desktop/shopLaptop/include/gioithieu.php
@@ -0,0 +1,34 @@
+<style type="text/css">
+<!--
+.style1 {
+	color: #0000FF;
+	font-weight: bold;
+}
+.style2 {
+	color: #FF0000;
+	font-weight: bold;
+}
+.style8 {
+	font-size: 18px;
+	color: #FF00FF;
+	font-weight: bold;
+}
+-->
+</style>
+<div align="justify" style="width:550;line-height:20px">
+<div align="center">
+<div align="center" style="line-height:25px ">
+ <span class="style1"><h2>INF205 - ĐIỆN TOÁN ĐÁM MÂY - ĐỒ ÁN LẬP TRÌNH WEB</h2></span></div>  
+   <div class="style8" align="center"><br />
+  WEBSITE KINH DOANH MÁY TÍNH TRỰC TUYẾN</div>
+   <p><br>
+  <span class="style2">Chuyên kinh doanh các dòng laptop từ các thương hiệu nổi tiếng với mức giá rẻ nhất thị trường và chất lượng hàng đầu.</span></p>
+   <p><br>
+   </p>
+  <div align="right"><span class="style1"><br>
+    <br>  
+    <br>
+</span></div>
+ 
+
+</div>
